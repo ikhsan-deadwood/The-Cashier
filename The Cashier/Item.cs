ff10a0c6e216083bfb2b84b65aa0de1d855ad7e6
ikhsan-deadwood <ikhsan.2775@students.amikom.ac.id>
2020-10-21 15:50:28 +0700

    Add project files.

---
+++ The Cashier/Item.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Cashier
+{
+    class Item
+    {
+        private int id;
+        public string title { get; set;}
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public double subtotal { get; set; }
+        private string type;
+        private int v;
+
+        public Item(int id, string title, int quantity, double price, double subtotal, string type)
+        {
+            this.id = id;
+            this.title = title;
+            this.quantity = quantity;
+            this.price = price;
+            this.subtotal = subtotal;
+            this.type = type;
+        }
+
+        public Item(int v, string title, int quantity, string type, double price)
+        {
+            this.v = v;
+            this.title = title;
+            this.quantity = quantity;
+            this.type = type;
+            this.price = price;
+        }
+
+        public string getTitle()
+        {
+            return title;
+        }
+        public int getquantity()
+        {
+            return quantity;
+        }
+        public double getprice()
+        {
+            return price;
+        }
+        public double getSubTotal()
+        {
+            subtotal = price * quantity;
+            return subtotal;
+        }     
+    }
+}
